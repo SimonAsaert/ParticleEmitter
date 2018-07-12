a4a8d3b1eabf699006612b80330e4f1b68bf4b18
SimonA <simon.asaert@gmail.com>
2018-07-12 19:04:15 +0100

    - Added functionality to summon invisible bat (currently invisibility disabled)  - Added command function to give user a summoning stick  - Added conversations API to allow user to define position for bat (first interaction complete in CustomInventory)  - Added CustomInventory that opens when bats with a specific meta value (particles) is right clicked  - Ensured bats with specific meta value (particles) cannot be killed by user by left click. Only can remove bats by menu options.  -

--- src/tld/sima/ParticlePlugin/Main.java
+++ src/tld/sima/ParticlePlugin/Main.java
@@ -3,19 +3,27 @@ package tld.sima.ParticlePlugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import net.md_5.bungee.api.ChatColor;
+import tld.sima.Command.Commands;
+import tld.sima.ParticlePlugin.Events.EventClass;
 
 public class Main extends JavaPlugin{
-	
+
+	private Commands commands = new Commands();
+
 	@Override
 	public void onEnable() {
-
-		getServer().getConsoleSender().sendMessage(ChatColor.BLUE + "\r\nParticle Plugin has been enabled\r\n");
 		
+		getCommand(commands.cmd1).setExecutor(commands);
+		getServer().getPluginManager().registerEvents(new EventClass(), this);
+		
+		getServer().getConsoleSender().sendMessage(ChatColor.BLUE + "Particle Plugin has been enabled\r\n");
 	}
-	
+
 	@Override
 	public void onDisable() {
 		
 	}
+	
+	
 	
 }
